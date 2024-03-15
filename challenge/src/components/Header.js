bb055a74d6822efc1c8e7bf39b8be750d2ade1ad
Fajar Saputro Juliantoro <fajarthea5@gmail.com>
2024-03-16 03:49:54 +0700

    Add redux toolkit to challenge

--- challenge/src/components/Header.js
+++ challenge/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
 
   return (
     <header className={classes.header}>
-      <h1>Redux Auth</h1>
+      <h1>Dibimbing Redux Auth</h1>
       {isAuth && (
         <nav>
           <ul>
